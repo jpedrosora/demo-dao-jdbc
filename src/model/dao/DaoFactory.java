01b70563fcb5425d34d89527e77d18ff9c8629f6
jpedrosora <pedro.rissardi@gmail.com>
2025-06-08 00:53:49 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+//classe que ira conter operações estaticas para implementar os daos
+	
+	//a classe ira expor um metodo que retorna o tipo da interface, mas internamente instanciara a implementação
+	//protege a implementacao e deixa somente a interface
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+	
+	
+}
